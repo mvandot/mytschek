0c77b249042363fcf193b6924268da030b0f5aac
mvandot <marija@vandot.rs>
2025-02-11 23:30:12 +0100

    3rd party script added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,10 +6,11 @@ export const Hero = () => {
         <div className='text-center mx-auto flex flex-col'>
             <h1 className='text-6xl font-semibold text-[#928490] md:h-16 bg-opacity-25 rounded md:mt-15 mt-40'>t-shirts in preparation</h1>
             <p className='hidden md:flex justify-center font-light text-3xl mt-5'>Secure yourself the first pieces</p>
-            <div className='flex md:flex-row flex-col justify-center md:gap-3'>
+            {/* <div className='flex md:flex-row flex-col justify-center md:gap-3'>
               <input class="h-full mx-5 py-3 px-3 min-w-[200px] mt-17 md:w-full bg-transparent placeholder:text-slate-700 text-sm text-slate-800 border-b-3 transition duration-300 ease focus:outline-none focus:border-[#F3E8FC] hover:border-[#F3E8FC]" placeholder="your email address" />
               <a href='#contact' className='px-2 py-2 h-full w-[200px] mt-15 mx-auto border-4 border-[#343434]-100 text-xl transition duration-300 ease focus:outline-none focus:border-[#F3E8FC] hover:border-[#F3E8FC] shadow-sm focus:shadow'>SIGN UP</a>
-            </div>
+            </div> */}
+            <span data-sumome-listbuilder-embed-id="3bf7c433b330618be83b64c1024e76b2b75d60baa782d225cab20fb49dca4fe1"></span>
         </div>
     </section>
   )
