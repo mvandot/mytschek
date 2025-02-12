0b694ed7683d7441d24452aa74c3ff873d2924ce
mvandot <marija@vandot.rs>
2025-02-12 15:22:40 +0100

    alt tags added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ export const About = () => {
       <div className="flex flex-col gap-10 sm:flex-col sm:justify-center md:flex-row md:justify-center md:mx-auto lg:flex-row lg:justify-center lg:mx-auto align-items">
         <div className="flex flex-col gap-2 h-full">
           <div className="mx-auto md:w-48 lg:w-64 xl:w-80">
-            <img src="assets/about/cotton.png" />
+            <img src="assets/about/cotton.png" alt="t-shirts crafted out of extremly soft brushed turkish cotton"/>
           </div>
           <div className="w-[280px] mx-auto md:w-48 lg:w-64 xl:w-70 xl:mx-0">
             <h2 className="text-2xl">quality</h2>
@@ -21,7 +21,7 @@ export const About = () => {
         </div>
         <div className="flex flex-col gap-2 h-full">
           <div className="mx-auto md:w-48 lg:w-64 xl:w-80">
-            <img src="assets/about/design.jpg" />
+            <img src="assets/about/design.jpg" alt="carefully and precisely sewn t-shirts to fit well and look polished" />
           </div>
           <div className="w-[280px] mx-auto md:w-48 lg:w-64 xl:w-70 xl:mx-0">
             <h2 className="text-2xl">design</h2>
@@ -50,7 +50,7 @@ export const About = () => {
         </div> */}
         <div className="flex flex-col gap-2 h-full">
           <div className="mx-auto md:w-48 lg:w-64 xl:w-80">
-            <img src="assets/about/models.jpg" />
+            <img src="assets/about/models.jpg" alt="5 essential t-shirt styles" />
           </div>
           <div className="w-[280px] mx-auto md:w-48 lg:w-64 xl:w-70 xl:mx-0">
             <h2 className="text-2xl">models</h2>
--- src/components/Brand.jsx
+++ src/components/Brand.jsx
@@ -5,7 +5,7 @@ export const Brand = () => {
     <section id='brand' className='mt-20'>
         <hr className='border-1'/>
         <div className='py-5 px-5 text-center'>
-            <p>mytschek /majček/ is one of my dearest nicknames written as a combination of english & german. 
+            <p>mytschek /majček/ is one of my dearest nicknames written as a german-english mixture. 
               it's also a deminutive form of a word t-shirt in my mother tongue language.
             </p>
         </div>
