85a52976c5d82c47e83e329b82e6e03101244f2c
mvandot <marija@vandot.rs>
2025-02-12 11:44:12 +0100

    heading color changed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ export const Hero = () => {
   return (
     <section className='w-full h-[794px] flex items-center justify-center bg-[url(../../assets/hero/hero.png)] bg-center bg-no-repeat'>
         <div className='text-center mx-auto flex flex-col'>
-            <h1 className='text-6xl font-semibold text-[#928490] md:h-16 bg-opacity-25 rounded md:mt-15 mt-40'>t-shirts in preparation</h1>
+            <h1 className='text-6xl font-semibold text-[#eaa18e] md:h-16 bg-opacity-25 rounded md:mt-15 mt-40'>t-shirts in preparation</h1>
             <p className='hidden md:flex justify-center font-light text-3xl mt-5'>Secure yourself the first pieces</p>
             {/* <div className='flex md:flex-row flex-col justify-center md:gap-3'>
               <input class="h-full mx-5 py-3 px-3 min-w-[200px] mt-17 md:w-full bg-transparent placeholder:text-slate-700 text-sm text-slate-800 border-b-3 transition duration-300 ease focus:outline-none focus:border-[#F3E8FC] hover:border-[#F3E8FC]" placeholder="your email address" />
